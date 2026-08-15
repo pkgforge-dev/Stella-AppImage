f6bb3ef2d7c576fa2cdc57da5b87de85a3124c18
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:43:21 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,7 +16,5 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /usr/bin/stella
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
